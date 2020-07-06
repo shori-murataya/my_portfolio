d342c1a8c67521456138a96d2d923ee7d589021d
shori-murata <mura.shori.king.king@gamil.com>
2020-07-07 00:21:18 +0900

    a little change

--- slick/common.js
+++ slick/common.js
@@ -2,6 +2,6 @@ $('.slide').slick({
     autoplay: true,
     adaptiveHeight: true,
     arrows: true,
-    prevArrow:'<div class="prev">◁</div>',
-    nextArrow:'<div class="next">▷</div>'
+    prevArrow:'<div class="prev" style="padding-right:10px;">◁</div>',
+    nextArrow:'<div class="next" style="padding-left:10px;">▷</div>'
 });
